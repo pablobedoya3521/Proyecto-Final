2816ddab21553800c941b90ba7a462c394634f45
Agudelo <pablobedoya3521@gmail.com>
2024-10-24 15:13:35 -0500

    ...

--- JavaApplication39/src/Vista/VentanaSeleccion.java
+++ JavaApplication39/src/Vista/VentanaSeleccion.java
@@ -4,6 +4,10 @@
  */
 package Vista;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatHighContrastIJTheme;
+import javax.swing.UIManager;
+
 /**
  *
  * @author sebastian
@@ -241,22 +245,12 @@ public class VentanaSeleccion extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaSeleccion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaSeleccion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaSeleccion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaSeleccion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+         try {
+              UIManager.setLookAndFeel( new FlatLightLaf() );
+              FlatHighContrastIJTheme.setup();
+                } catch( Exception ex ) {
+                    System.err.println( "Failed to initialize LaF" );
+}
         //</editor-fold>
 
         /* Create and display the form */
